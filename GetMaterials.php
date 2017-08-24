96bf9a71a020d139a17c50c3eb2f47b3a89592eb
root <root@swteam.XJETDOM.local>
2017-08-24 14:01:40 +0300

    Table view

--- GetMaterials.php
+++ GetMaterials.php
@@ -12,37 +12,45 @@ $FormulationsList=[];
 // select database
 mysqli_select_db($con,"ChemistryTest");
 
-$sql="SELECT A.*,C.GroupType from Materials as A , FormulationNameToGroup as B , FormulationGroup as C where A.Type=\"$SBMatiralType\" and A.Name=B.Name and B.Id=C.Id";
+if(!empty($_GET["GroupType"]))
+  $sql="SELECT A.*,C.GroupType from Materials as A , FormulationNameToGroup as B , FormulationGroup as C where A.Type=\"$SBMatiralType\" and A.Name=B.Name and B.Id=C.Id and Available=1";
+else
+  $sql="SELECT A.* from Materials as A  where A.Type=\"$SBMatiralType\" and Available=1";
 $result = mysqli_query($con,$sql);
 $outp = array();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
 foreach ($outp as $value)
 {
-	$sql=sprintf("SELECT A.Material2,A.Id from  MaterialsRecipe AS A,Materials as B where Material1='%s' and B.Type='Raw' and A.Material2=B.Name",$value['Name']);
-        $resultRaw = mysqli_query($con,$sql);
-	$outpRaw = array();
-	$outpRaw = $resultRaw->fetch_all(MYSQLI_ASSOC);
-	$value['Raw'] = [];
-	$index =0;
-	foreach ($outpRaw as $valueRaw)
+	if(!empty($_GET["Raw"]))
 	{
-		$value['Raw'][$index] = $valueRaw['Material2'];
-		$index++;
+		$sql=sprintf("SELECT A.Material2,A.Id from  MaterialsRecipe AS A,Materials as B where Material1='%s' and B.Type='Raw' and A.Material2=B.Name",$value['Name']);
+		$resultRaw = mysqli_query($con,$sql);
+		$outpRaw = array();
+		$outpRaw = $resultRaw->fetch_all(MYSQLI_ASSOC);
+		$value['Raw'] = [];
+		$index =0;
+		foreach ($outpRaw as $valueRaw)
+		{
+			$value['Raw'][$index] = $valueRaw['Material2'];
+			$index++;
+		}
 	}
 
-
-	$sql=sprintf("SELECT A.Material2,A.Id from  MaterialsRecipe AS A,Materials as B where Material1='%s' and B.Type='Master' and A.Material2=B.Name",$value['Name']);
-        $resultMaster = mysqli_query($con,$sql);
-	$outpMaster = array();
-	$outpMaster = $resultMaster->fetch_all(MYSQLI_ASSOC);
-	$value['Master'] = [];
-	$index =0;
-	foreach ($outpMaster as $valueMaster)
+	if(!empty($_GET["Master"]))
 	{
-		$value['Master'][$index] = $valueMaster['Material2'];
-		$index++;
+		$sql=sprintf("SELECT A.Material2,A.Id from  MaterialsRecipe AS A,Materials as B where Material1='%s' and B.Type='Master' and A.Material2=B.Name",$value['Name']);
+		$resultMaster = mysqli_query($con,$sql);
+		$outpMaster = array();
+		$outpMaster = $resultMaster->fetch_all(MYSQLI_ASSOC);
+		$value['Master'] = [];
+		$index =0;
+		foreach ($outpMaster as $valueMaster)
+		{
+			$value['Master'][$index] = $valueMaster['Material2'];
+			$index++;
+		}
 	}
-        array_push($FormulationsList,$value);
+	array_push($FormulationsList,$value);
 }
 $FList =  json_encode($FormulationsList);
 echo $FList;
--- Material.js
+++ Material.js
@@ -1,53 +1,72 @@
-function PrintTable(response){  
+function PrintTable(response , FType , Masters , Raws){  
   var jsonObject = JSON.parse(response);
-	        var x ;
-			$('#mytable').append('<thead>' +
-				'<th>Formulation Name</th>' +
-				'<th>Formulation Type</th>' +
-				'<th>Masters</th>' +
-				'<th>Raws</th>' +
-				'<th>Comment</th>'+
-				'<th>Edit By</th>' +
-				'<th>Last Edit</th>' +
-				'<th>Edit</th>' +
-				'<th>Delete</th>' +
-				'<th>Prodaction</th>' +
-				'<th>New</th>'+
-				'</thead>');
+	var x ;
+	var theadStr =  '<thead><th>Name</th>';
+	if (FType)
+		theadStr += '<th>Type</th>';
+	if (Masters)
+		theadStr += '<th>Masters</th>';
+	if (Raws)
+		theadStr += '<th>Raws</th>';
+	theadStr +=
+		'<th>Comment</th>'+
+		'<th>Edit By</th>' +
+		'<th>Last Edit</th>' +
+		'<th>Edit</th>' +
+		'<th>Delete</th>' +
+		'<th>Prodaction</th>' +
+		'<th>New</th>'+
+		'</thead>';
+	$('#mytable').append(theadStr);
         
                 for (x in jsonObject) {
 			var txt='<tr><td>';
 			Now = new Date($.now());
 			RawStr="";
 			MasterStr="";
-			delimiter="";
-			for (i in jsonObject[x].Raw) {
-				RawStr+=delimiter + jsonObject[x].Raw[i];
-				delimiter=",";
-			}
-			delimiter="";
-			for (i in jsonObject[x].Master) {
-				MasterStr+=delimiter +  jsonObject[x].Master[i];
-				delimiter=",";
-			}
 
+			if (Raws)
+			{
+				delimiter="";
+				for (i in jsonObject[x].Raw) {
+					RawStr+=delimiter + jsonObject[x].Raw[i];
+					delimiter=",";
+				}
+			}
+			if (Masters)
+			{
+				delimiter="";
+				for (i in jsonObject[x].Master) {
+					MasterStr+=delimiter +  jsonObject[x].Master[i];
+					delimiter=",";
+				}
+			}
 
 			txt += jsonObject[x].Name;
 			txt += '</td><td>';
-			txt += jsonObject[x].GroupType;
-			txt += '</td><td>';
-			txt += MasterStr;
-			txt += '</td><td>';
-			txt += RawStr;
-			txt += '</td><td>';
+			if (FType)
+			{
+				txt += jsonObject[x].GroupType;
+				txt += '</td><td>';
+			}
+			if (Masters)
+			{
+				txt += MasterStr;
+				txt += '</td><td>';
+			}
+			if (Raws)
+			{
+				txt += RawStr;
+				txt += '</td><td>';
+			}
 			txt += jsonObject[x].Comment;
 			txt += '</td><td>';
 			txt += "Yair";
 			txt += '</td><td>';
 			txt += Now.getHours() + ":" + Now.getMinutes() + ":" + Now.getSeconds();
 			txt += '</td>';
-			txt += '<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>';
-			txt += '<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>';
+			txt += '<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" id="llaa"><span class="glyphicon glyphicon-pencil"></span></button></p></td>';
+			txt += '<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" data-yourparameter=' + jsonObject[x].Name + '><span class="glyphicon glyphicon-trash"></span></button></p></td>';
 			txt += '<td><p data-placement="top" data-toggle="tooltip" title="Prodaction"><button class="btn btn-success btn-xs" data-title="Prodaction" data-toggle="modal" data-target="#instance" ><span class="glyphicon glyphicon-filter"></span></button></p></td>';
 
 			txt += '<td></td>';
@@ -68,3 +87,5 @@ function PrintTable(response){
                                       '</tr>');
 
 }  
+
+
