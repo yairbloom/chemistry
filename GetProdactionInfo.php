ee69a4bd74daedb9c13182d8ddc861a101f55567
Yair Bloom <yairbloom@gmail.com>
2017-09-11 17:27:03 +0300

    Inserting Production logic

---
+++ GetProdactionInfo.php
@@ -0,0 +1,56 @@
+<?php
+// connect mysql server
+$con = mysqli_connect('localhost','chem','mistry','ChemistryTest');
+if (!$con) { 
+    die('Could not connect: ' . mysql_error()); 
+} 
+
+
+$MatiralName = $_GET['MatiralName'];
+$ProductionList=[];
+
+// select database
+mysqli_select_db($con,"ChemistryTest");
+
+$sql="SELECT SerialNumber,Comment,Quantity,QuantityType,LastModify from ProductionMaterials where Name='".$MatiralName."'";
+$result = mysqli_query($con,$sql);
+$outp = array();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+foreach ($outp as $value)
+{
+	if(!empty($_GET["Raw"]))
+	{
+		$sql=sprintf("select A.Material2SN as MaterialSN ,B.Material2 as MaterialName from ProductionRecipe as A,MaterialsRecipe as B, Materials as C where A.Material1SN='%s' and A.Id=B.Id and B.Material2=C.Name and C.Type='Raw'",$value['SerialNumber']);
+		$resultRaw = mysqli_query($con,$sql);
+		$outpRaw = array();
+		$outpRaw = $resultRaw->fetch_all(MYSQLI_ASSOC);
+		$value['Raw'] = [];
+		$index =0;
+		foreach ($outpRaw as $valueRaw)
+		{
+			$value['Raw'][$index] = $valueRaw;
+			$index++;
+		}
+	}
+
+	if(!empty($_GET["Master"]))
+	{
+		$sql=sprintf("select A.Material2SN as MaterialSN ,B.Material2 as MaterialName from ProductionRecipe as A,MaterialsRecipe as B, Materials as C where A.Material1SN='%s' and A.Id=B.Id and B.Material2=C.Name and C.Type='Master'",$value['SerialNumber']);
+		$resultMaster = mysqli_query($con,$sql);
+		$outpMaster = array();
+		$outpMaster = $resultMaster->fetch_all(MYSQLI_ASSOC);
+		$value['Master'] = [];
+		$index =0;
+		foreach ($outpMaster as $valueMaster)
+		{
+			$value['Master'][$index] = $valueMaster;
+			$index++;
+		}
+
+	}
+	array_push($ProductionList,$value);
+
+}
+echo json_encode($ProductionList );
+?>
+
--- Material.js
+++ Material.js
@@ -1,4 +1,3 @@
-
 function PrintTable(response , FType , Masters , Raws , MatiralType){  
   var MatiralsJson = JSON.parse(response);
   var MatList = MatiralsJson.MatList;
@@ -80,7 +79,7 @@ function PrintTable(response , FType , Masters , Raws , MatiralType){
 			txt += '<span class="glyphicon glyphicon-pencil"></span></button>';
 			txt += '<button class="btn btn-danger btn-xs mybtn-delete" data-title="Delete" data-toggle="modal" data-target="#DeleteMatiral"';
 			txt += ' data-placement="top" data-toggle="tooltip" title="Delete"><span class="glyphicon glyphicon-trash"></span></button>';
-			txt += '<button class="btn btn-success btn-xs" data-title="Prodaction" data-toggle="modal" data-target="#Prodaction" data-placement="top" data-toggle="tooltip" title="Prodaction">';
+			txt += '<button class="btn btn-success btn-xs mybtn-prodaction" data-title="Prodaction"  data-toggle="tooltip" title="Prodaction">';
 			txt += '<span class="glyphicon glyphicon-filter"></span></button></td>';
 			txt += '<td></td>';
 			$('#mytable').append(txt);
@@ -93,6 +92,7 @@ function PrintTable(response , FType , Masters , Raws , MatiralType){
 		$("#DelSpanHeadline").text("Deleting  " + MatiralName);
 		$("#DeleteMatiral").attr("data-MatiralName" , MatiralName);
          })
+
 	$(".mybtn-new").click(function(){
 		var GroupType = MatiralsJson.GroupType;
 		var MasterOptions = MatiralsJson.MasterOptions;
@@ -180,6 +180,193 @@ function PrintTable(response , FType , Masters , Raws , MatiralType){
 
 	})
 
+      $(".mybtn-prodaction").click(function(){
+	      var MatiralName = $(this).parentsUntil("tr").parent().find("td:eq(0)").text();
+	      var MatiralType = $("#MainDiv").attr("data-MatiralType");
+	      $("#MainDiv").attr("data-MatiralName" , MatiralName);
+	      $("#MyHeadline").text(MatiralType + ": " + MatiralName);
+	      $("#mytable").load("Production.html");
+
+         })
+}  
+
+
+function PrintProductionTable(response , Raws , Masters){  
+  var PJson = JSON.parse(response);
+	var x ;
+	var theadStr =  '<thead><th>SerialId</th>';
+	if (Masters)
+		theadStr += '<th>Masters</th>';
+	if (Raws)
+		theadStr += '<th>Raws</th>';
+	theadStr +=
+		'<th>Quantity</th>' +
+		'<th>Comment</th>'+
+		'<th>Edit By</th>' +
+		'<th>Last Edit</th>' +
+		'<th>Actions</th>' +
+		'<th>New</th>'+
+		'</thead>';
+	$('#mytable').append(theadStr);
+	        var NewItemRow='<tr><td>';
+	        if (Raws) 
+		  NewItemRow+='</td><td>';
+	        if (Masters) 
+		  NewItemRow+='</td><td>';
+	        NewItemRow+='<td></td><td></td><td></td><td></td><td></td><td><p data-placement="top" data-toggle="tooltip" title="New">';
+	        NewItemRow+='<button class="btn btn btn-xs mybtn-new" data-title="New" data-toggle="modal" data-target="#NewMatiral" ><span class="glyphicon glyphicon-plus"></span></button></p></td></tr>';
+	         $('#mytable').append(NewItemRow);
+
+                for (x in PJson) {
+			var txt='<tr><td>';
+			RawStr="";
+			MasterStr="";
+
+			if (Raws)
+			{
+				delimiter="";
+				for (i in PJson[x].Raw) {
+					RawStr+=delimiter + PJson[x].Raw[i].MaterialName + '(' + PJson[x].Raw[i].MaterialSN + ')';
+					delimiter=",";
+				}
+			}
+			if (Masters)
+			{
+				delimiter="";
+				for (i in PJson[x].Master) {
+					MasterStr+=delimiter +  PJson[x].Master[i].MaterialName + '(' + PJson[x].Master[i].MaterialSN + ')';
+					delimiter=",";
+				}
+			}
+
+			txt += PJson[x].SerialNumber;
+			txt += '</td><td>';
+			if (Masters)
+			{
+				txt += MasterStr;
+				txt += '</td><td>';
+			}
+			if (Raws)
+			{
+				txt += RawStr;
+				txt += '</td><td>';
+			}
+
+			txt += PJson[x].Quantity;
+			if (PJson[x].QuantityType == 1)
+				txt += " Gram";
+			else if (PJson[x].QuantityType == 2)
+                                txt += " Liter";
+			txt += '</td><td>';
+			txt += PJson[x].Comment;
+			txt += '</td><td>';
+			txt += "Yair";
+			txt += '</td><td>';
+			txt += PJson[x].LastModify;
+			txt += '</td>';
+			txt += '<td><div class="btn-group">';
+		        txt += '<button class="btn btn-primary btn-xs mybtn-edit" data-title="Edit" data-toggle="modal" data-target="#EditMatiral" data-placement="top" data-toggle="tooltip" title="Edit">';
+			txt += '<span class="glyphicon glyphicon-pencil"></span></button>';
+			txt += '<button class="btn btn-danger btn-xs mybtn-delete" data-title="Delete" data-toggle="modal" data-target="#DeleteMatiral"';
+			txt += ' data-placement="top" data-toggle="tooltip" title="Delete"><span class="glyphicon glyphicon-trash"></span></button>';
+			txt += '<td></td>';
+			$('#mytable').append(txt);
+                 }
+
+        $(".mybtn-delete").click(function(){
+
+		var MatiralName = $(this).parentsUntil("tr").parent().find("td:eq(0)").text();
+ 		$("#MyDelSpan").html("    Are you sure you want to delete  " + MatiralName);
+		$("#DelSpanHeadline").text("Deleting  " + MatiralName);
+		$("#DeleteMatiral").attr("data-MatiralName" , MatiralName);
+         })
+
+	$(".mybtn-new").click(function(){
+		var GroupType = MatiralsJson.GroupType;
+		var MasterOptions = MatiralsJson.MasterOptions;
+		var RawOptions = MatiralsJson.RawOptions;
+		var OptionList =''; 
+		$("#NewMatiralHeading").text('New '+ $("#MenuContent").attr("data-Matiral-type"));
+
+		if (FType) {
+			$("#NewGroupsTypeDiv").removeClass('hidden');
+			for (x in GroupType) 
+				OptionList+='<option value=' +  GroupType[x].Id + '>' + GroupType[x].GroupType + '</option>';
+			$("#NewGroupsTypeSelect").html(OptionList);
+		}
+                if (Masters) {
+			$("#NewMasterDiv").removeClass('hidden');
+			$("#NewMasterSelect").val('');
+			for (x in MasterOptions) 
+				$("#NewMasterSelect").append('<option>' + MasterOptions[x].Name + '</option>');
+			$("#NewMasterSelect").trigger("chosen:updated");
+		}
+		if (Raws) {
+			$("#NewRawDiv").removeClass('hidden');
+			$("#NewRawSelect").val('');
+			for (x in RawOptions) 
+				$("#NewRawSelect").append('<option>' + RawOptions[x].Name + '</option>');
+			$("#NewRawSelect").trigger("chosen:updated");
+		}
+
+	})
+	$(".mybtn-edit").click(function(){
+		var GroupType = MatiralsJson.GroupType;
+		var MasterOptions = MatiralsJson.MasterOptions;
+		var RawOptions = MatiralsJson.RawOptions;
+		var OptionList =''; 
+		var OldName = $(this).parentsUntil("tr").parent().find("td:eq(0)").text();
+		var CurrentGroupType = $(this).parentsUntil("tr").parent().find("td:eq(1)").text();
+		var CurrentMasterList = $(this).parentsUntil("tr").parent().find("td:eq(2)").text().split(',');
+		var CurrentRawList = $(this).parentsUntil("tr").parent().find("td:eq(3)").text().split(',');
+		var CurrentComment = $(this).parentsUntil("tr").parent().find("td:eq(4)").text();
+
+		$("#EditMatiral").attr("data-MatiralName" , OldName);
+		$("#EditMatiralHeading").text('Edit '+ OldName);
+		$("#EditMatiralName").val(OldName);
+		$("#EditMatiralComments").val(CurrentComment);
+
+		if (FType) {
+			$("#EditGroupsTypeDiv").removeClass('hidden');
+			for (x in GroupType) 
+			{
+				if (CurrentGroupType == GroupType[x].GroupType)
+					OptionList+='<option value=' +  GroupType[x].Id + ' selected>' + GroupType[x].GroupType + '</option>';
+				else
+					OptionList+='<option value=' +  GroupType[x].Id + '>' + GroupType[x].GroupType + '</option>';
+			}
+			$("#EditGroupsTypeSelect").html(OptionList);
+		}
+                if (Masters) {
+			$("#EditMasterDiv").removeClass('hidden');
+			$("#EditMasterSelect").val('');
+			for (x in MasterOptions) 
+			{
+				if(jQuery.inArray(MasterOptions[x].Name, CurrentMasterList) !== -1)
+					$("#EditMasterSelect").append('<option selected>' + MasterOptions[x].Name + '</option>');
+				else
+					$("#EditMasterSelect").append('<option>' + MasterOptions[x].Name + '</option>');
+			}
+			$("#EditMasterSelect").trigger("chosen:updated");
+		}
+		if (Raws) {
+			$("#EditRawDiv").removeClass('hidden');
+			$("#EditRawSelect").val('');
+			for (x in RawOptions) 
+			{
+				if(jQuery.inArray(RawOptions[x].Name, CurrentRawList) !== -1)
+					$("#EditRawSelect").append('<option selected>' + RawOptions[x].Name + '</option>');
+				else
+					$("#EditRawSelect").append('<option>' + RawOptions[x].Name + '</option>');
+			}
+			$("#EditRawSelect").trigger("chosen:updated");
+		}
+
+
+
+
+
+	})
 }  
 
 
